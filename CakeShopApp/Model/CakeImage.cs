d30cea2c2f3b5fee678272a7c910f41378834e19
Vĩ Bùi <buivankhang9@gmail.com>
2021-01-07 18:17:15 +0700

    Thêm các class bánh, đơn hàng, thông tin cơ bản của bánh và hình ảnh bánh.

---
+++ CakeShopApp/Model/CakeImage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class CakeImage : INotifyPropertyChanged
+{
+	private string imagePath;
+	public string ImagePath
+	{
+		get
+		{
+			return imagePath;
+		}
+		set
+		{
+			imagePath = value;
+			OnPropertyChanged("ImagePath");
+		}
+	}
+
+	public CakeImage()
+	{
+		this.imagePath = "";
+	}
+	public CakeImage(string filename)
+	{
+		this.imagePath = filename;
+	}
+
+	#region INotifyPropertyChanged Members  
+
+	public event PropertyChangedEventHandler PropertyChanged;
+	private void OnPropertyChanged(string propertyName)
+	{
+		if (PropertyChanged != null)
+		{
+			PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+
+	#endregion
+}
