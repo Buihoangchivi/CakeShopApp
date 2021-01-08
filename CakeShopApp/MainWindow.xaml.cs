5e1278bb01a6b971b78a1e352bde5643bf598406
Tan Pham <seacaboqn@gmail.com>
2021-01-08 18:22:50 +0700

    Thêm màn hình AddScreen
    Thêm màn hình AddScreen

--- CakeShopApp/MainWindow.xaml.cs
+++ CakeShopApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,11 @@ namespace CakeShopApp
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+			
 		}
 
 		private void Cake_Click(object sender, RoutedEventArgs e)
@@ -94,5 +100,154 @@ namespace CakeShopApp
 		{
 
 		}
-	}
+
+        private void CancelTripButton_Click(object sender, RoutedEventArgs e)
+        {
+			////Đóng màn hình thêm chuyến đi
+			//AddTripGrid.Visibility = Visibility.Collapsed;
+			////Tắt màu của nút Add
+			//var wrapPanel = (WrapPanel)AddTripButton.Content;
+			//var collection = wrapPanel.Children;
+			//var block = (TextBlock)collection[0];
+			//var text = (TextBlock)collection[2];
+			//block.Background = Brushes.Transparent;
+			//text.Foreground = Brushes.Black;
+
+			//if (isEditMode == true)
+			//{
+			//	//Quay ve man hinh chi tiet
+			//	DetailTripGrid.DataContext = TripInfoList[selectedTripIndex];
+			//	DetailTripGrid.Visibility = Visibility.Visible;
+			//	ControlStackPanel.Visibility = Visibility.Visible;
+
+			//	//Tắt chế độ chỉnh sửa
+			//	isEditMode = false;
+			//	IsDetailTrip = true;
+			//}
+			//else
+			//{
+			//	//Quay về màn hình Home
+			//	clickedControlButton = HomeButton;
+			//	TripListGrid.Visibility = Visibility.Visible;
+			//	TypeBarDockPanel.Visibility = Visibility.Visible;
+			//	ControlStackPanel.Visibility = Visibility.Visible;
+			//}
+
+			////Hiển thị màu cho nút Home
+			//wrapPanel = (WrapPanel)HomeButton.Content;
+			//collection = wrapPanel.Children;
+			//block = (TextBlock)collection[0];
+			//text = (TextBlock)collection[2];
+			//block.Background = (SolidColorBrush)new BrushConverter().ConvertFromString(ColorScheme);
+			//text.Foreground = block.Background;
+
+			//clickedControlButton = HomeButton;
+
+			////Cập nhật lại giao diện
+			//UpdateUIFromData();
+		}
+
+      
+
+        private void AddImageButton_Click(object sender, RoutedEventArgs e)
+        {
+			//var fileDialog = new OpenFileDialog();
+			//fileDialog.Multiselect = true;
+			//fileDialog.Filter = "Image Files(*.JPG*;*.JPEG*;*.PNG*)|*.JPG;*.JPEG*;*.PNG*";
+			//fileDialog.Title = "Select Image";
+
+			//if (fileDialog.ShowDialog() == true)
+			//{
+			//	var fileNames = fileDialog.FileNames;
+			//	foreach (var filename in fileNames)
+			//	{
+			//		trip.ImagesList.Add(new TripImage(filename));
+			//	}
+			//}
+		}
+
+      
+
+        private void DeleteImageButton_Click(object sender, RoutedEventArgs e)
+        {
+			//trip.ImagesList.Remove(ImagesListView.SelectedItem as TripImage);
+		}
+
+      
+
+       
+
+        private void SaveTripButton_Click(object sender, RoutedEventArgs e)
+        {
+			//if (isEditMode == false)
+			//{
+			//	string appFolder = GetAppDomain();
+			//	for (int i = 0; i < trip.ImagesList.Count; i++)
+			//	{
+			//		var imageExtension = System.IO.Path.GetExtension(trip.ImagesList[i].ImagePath);
+			//		var newImageName = $"Images/{trip.TripID}_{i}{imageExtension}";
+			//		var newPath = appFolder + newImageName;
+			//		File.Copy(trip.ImagesList[i].ImagePath, newPath, true);
+			//		trip.ImagesList[i].ImagePath = newImageName;
+			//	}
+			//	trip.PrimaryImagePath = trip.ImagesList[0].ImagePath;
+			//	TripInfoList.Add(trip);
+			//}
+			//else
+			//{
+			//	string appFolder = GetAppDomain();
+			//	for (int i = 0; i < trip.ImagesList.Count; i++)
+			//	{
+			//		TripImage currentImage = trip.ImagesList[i];
+			//		var imageExtension = System.IO.Path.GetExtension(currentImage.ImagePath);
+			//		var newImageName = $"Images/{trip.TripID}_{i}{imageExtension}";
+			//		var newPath = appFolder + newImageName;
+			//		if (System.IO.Path.IsPathRooted(currentImage.ImagePath))
+			//		{
+			//			File.Copy(currentImage.ImagePath, newPath, true);
+			//			trip.ImagesList[i].ImagePath = newImageName;
+			//		}
+			//		else
+			//		{
+			//			if (currentImage.ImagePath != TripInfoList[selectedTripIndex].ImagesList[i].ImagePath)
+			//			{
+			//				File.Delete(appFolder + TripInfoList[selectedTripIndex].ImagesList[i].ImagePath);
+			//				File.Move(appFolder + currentImage.ImagePath, newPath);
+			//				currentImage.ImagePath = newImageName;
+			//			}
+			//		}
+			//	}
+			//	if (trip.ImagesList.Count > 0)
+			//	{
+			//		trip.PrimaryImagePath = trip.ImagesList[0].ImagePath;
+			//	}
+			//	else
+			//	{
+			//		trip.PrimaryImagePath = "";
+			//	}
+			//	TripInfoList[selectedTripIndex] = trip;
+			//}
+
+			////Đóng giao diện thêm/chỉnh sửa và mở giao diện trang chủ
+			//CancelTripButton_Click(null, null);
+		}
+
+      
+
+  //      private void Description_LostFocus(object sender, RoutedEventArgs e)
+  //      {
+		//	if (string.IsNullOrWhiteSpace(Description.Text))
+		//	{
+		//		Description.Text = "Nhập văn bản ở đây...";
+		//	}
+		//}
+
+  //      private void Description_GotFocus(object sender, RoutedEventArgs e)
+  //      {
+		//	if (Description.Text == "Nhập văn bản ở đây...")
+		//	{
+		//		Description.Text = "";
+		//	}
+		//}
+    }
 }
